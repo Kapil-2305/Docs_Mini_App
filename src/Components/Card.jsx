a19b5b59a08d2cda7d62dd8fa23d48525d903894
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-12-22 23:43:30 +0530

    Add motion animation to Card component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { FaRegFileAlt } from 'react-icons/fa'
 import { LuDownload } from 'react-icons/lu'
 import { IoClose } from 'react-icons/io5'
+import {motion} from 'framer-motion'
 
-const Card = ({data}) => {
+const Card = ({ data, reference }) => {
     return (
-        <div className='relative w-60 h-72 flex-shrink-0 rounded-[30px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden'>
+        <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.2}} className='relative w-60 h-72 flex-shrink-0 rounded-[30px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden'>
             <FaRegFileAlt />
             <p className='text-sm leading-tight mt-5'>{data.desc}</p>
             <div className='footer absolute bottom-0 w-full left-0'>
@@ -24,7 +25,7 @@ const Card = ({data}) => {
                     )
                 }
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
-import {motion} from 'framer-motion'
 
 const Foreground = () => {
+    const ref = useRef(null);
+
     const data = [
         {
             desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
@@ -36,9 +37,9 @@ const Foreground = () => {
         }
     ]
     return (
-        <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+        <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
             {data.map((item, index) => (
-                <Card key={index} data={item} />
+                <Card key={index} data={item} reference={ref}/>
             ))}
         </div>
     )
