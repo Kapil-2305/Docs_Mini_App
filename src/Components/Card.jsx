dc960b61806cc95621c0b0b7302ba4a24020863e
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-12-22 23:30:03 +0530

    Refactor Card component styling and add conditional rendering for tag

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,7 +5,7 @@ import { IoClose } from 'react-icons/io5'
 
 const Card = ({data}) => {
     return (
-        <div className='relative w-60 h-72 rounded-[30px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden'>
+        <div className='relative w-60 h-72 flex-shrink-0 rounded-[30px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden'>
             <FaRegFileAlt />
             <p className='text-sm leading-tight mt-5'>{data.desc}</p>
             <div className='footer absolute bottom-0 w-full left-0'>
@@ -16,9 +16,13 @@ const Card = ({data}) => {
                     </span>
                 </div>
 
-                <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
-                    <h3 className='text-sm font-semibold'>Download Now</h3>
-                </div>
+                {
+                    data.tag.isOpen && (
+                        <div className={`tag w-full py-4 bg-${data.tag.tagColor} flex items-center justify-center`}>
+                            <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -6,16 +6,36 @@ const Foreground = () => {
         {
             desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
             filesize: ".9mb",
-            close: true,
+            close: false,
             tag: {
                 isOpen: true,
                 tagTitle: "Download Now",
-                tagColor: "green-600"
+                tagColor: "green-600",
+            }
+        },
+        {
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+            filesize: ".9mb",
+            close: true,
+            tag: {
+                isOpen: true,
+                tagTitle: "Save Now",
+                tagColor: "zinc-600",
+            }
+        },
+        {
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+            filesize: ".7mb",
+            close: true,
+            tag: {
+                isOpen: true,
+                tagTitle: "Delete Now",
+                tagColor: "red-600",
             }
         }
     ]
     return (
-        <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+        <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
             {data.map((item, index) => (
                 <Card key={index} data={item} />
             ))}
